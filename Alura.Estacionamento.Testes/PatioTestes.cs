5a301cb4ab7bc3c1dad0937f53c6c77adf816f52
Rafael Jáber <rafael.jaber@hotmail.com>
2022-12-18 00:11:44 -0300

    Implementado os testes da classe Patio, nos métodos ValidaFaturamento para automovél e motocicleta

---
+++ Alura.Estacionamento.Testes/PatioTestes.cs
@@ -0,0 +1,58 @@
+﻿using Alura.Estacionamento.Alura.Estacionamento.Modelos;
+using Alura.Estacionamento.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Alura.Estacionamento.Testes
+{
+    public class PatioTestes
+    {
+        [Fact]
+        public void ValidaFaturamentoAutomovel()
+        {
+            // Arrange
+            Patio estacionamento = new Patio();
+            Veiculo veiculo = new Veiculo();
+            veiculo.Proprietario = "Rafael Jáber";
+            veiculo.Tipo = TipoVeiculo.Automovel;
+            veiculo.Cor = "Azul";
+            veiculo.Modelo = "Fusca";
+            veiculo.Placa = "ASD-9999";
+            
+            estacionamento.RegistrarEntradaVeiculo(veiculo);
+            estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+
+            // Act
+            double faturamento = estacionamento.TotalFaturado();
+
+            // Assert
+            Assert.Equal(2, faturamento);
+        }
+
+        [Fact]
+        public void ValidaFaturamentoMotocicleta()
+        {
+            // Arrange
+            Patio estacionamento = new Patio();
+            Veiculo veiculo = new Veiculo();
+            veiculo.Proprietario = "Rafael Jáber";
+            veiculo.Tipo = TipoVeiculo.Motocicleta;
+            veiculo.Cor = "Azul";
+            veiculo.Modelo = "Biz";
+            veiculo.Placa = "ASD-9999";
+
+            estacionamento.RegistrarEntradaVeiculo(veiculo);
+            estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+
+            // Act
+            double faturamento = estacionamento.TotalFaturado();
+
+            // Assert
+            Assert.Equal(1, faturamento);
+        }
+    }
+}
